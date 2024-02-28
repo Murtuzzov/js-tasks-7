6f224405714f584f026095137330d90e44935fd4
murtuzzov <murtuzzov>
2024-02-28 23:21:41 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+//TASK 1
+// const numberArr = [1, 5, 4, 8, 9];
+
+// for (let i = 0; i < numberArr.length; i += 1) {
+//   numberArr[i] *= 2;
+// }
+// console.log(numberArr); // Выведет: [2, 10, 8, 16, 18]
+
+//TASK 2
+//	Создайте программу, которая находит сумму всех элементов числового массива.
+// const numberArr = [1, 5, 4, 8, 9];
+// let sum = 0;
+
+// for (let i = 0; i < numberArr.length; i += 1) {
+//   sum += numberArr[i];
+// }
+
+// console.log(sum);
+
+//TASK 3
+// const numberArr = [1, 5, 4, 8, 9];
+// let sum = 0;
+
+// for (let i = 0; i < numberArr.length; i += 1) {
+//   sum += numberArr[i];
+// }
+// const result = sum / numberArr.length;
+// console.log(result);
+
+//TASK 6
+// const numbers = [8, 7, 6, 5, 4, 3, 2, 1];
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] % 2 === 0) {
+//     console.log(numbers[i]);
+//   }
+// }
